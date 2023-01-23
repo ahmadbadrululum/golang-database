e21e5b803105fd9c31e2a18e7ef8e3af3eac81c7
Badrules <ahmadbadrul635@gmail.com>
2023-01-23 15:04:17 +0700

    quuery context untuk get data

--- sql_test.go
+++ sql_test.go
@@ -13,7 +13,7 @@ func TestExecSql(t *testing.T) {
 	ctx := context.Background()
 
 	// prepare statement
-	script := `INSERT INTO customer (id, name) VALUES ("bad", "rules")`
+	script := `INSERT INTO customer (id, name) VALUES ("bad1", "rules1")`
 
 	_, err := db.ExecContext(ctx, script)
 	if err != nil {
@@ -21,4 +21,36 @@ func TestExecSql(t *testing.T) {
 	}
 
 	fmt.Println("masuk")
+}
+
+func TestGetSql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	// prepare statement
+	script := `select * from customer`
+
+	rows, err := db.QueryContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, name string
+
+		// fmt.Println(rows.Scan())
+
+		err = rows.Scan(&id, &name)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("id " + id)
+		fmt.Println("name " + name)
+	}
+
 }
