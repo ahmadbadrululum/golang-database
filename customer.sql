6ac963eb3be98cf85fd5a278e553380e75db2e87
Badrules <ahmadbadrul635@gmail.com>
2023-01-25 20:19:17 +0700

    sample logic login dan sql injection

--- customer.sql
+++ customer.sql
@@ -5,3 +5,9 @@ add column status double default 0.0,
 add column created_at timestamp default current_timestamp,
 add column birth_date date,
 add column married boolean default false;
+
+create table user (
+    username varchar(100) not null,
+    password varchar(100) not null,
+    primary key (username)
+);
