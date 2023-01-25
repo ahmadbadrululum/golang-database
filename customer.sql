593231f0c17184d41e27a605f55b6de5b92d3a38
Badrules <ahmadbadrul635@gmail.com>
2023-01-25 19:29:42 +0700

    alter table customer

---
+++ customer.sql
@@ -0,0 +1,7 @@
+alter table customer
+add column email varchar(100),
+add column balance int default 0,
+add column status double default 0.0,
+add column created_at timestamp default current_timestamp,
+add column birth_date date,
+add column married boolean default false;
